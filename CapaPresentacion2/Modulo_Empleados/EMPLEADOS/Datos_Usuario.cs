ca1048217c16b4f22d3c8fa10856e39712ebe4d8
7559 <7559@MICHELALEXIS>
2017-04-25 12:59:30 -0600

    Cabkasdjfgkjahsdf

---
+++ CapaPresentacion2/Modulo_Empleados/EMPLEADOS/Datos_Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modulo_Empleados.EMPLEADOS
+{
+    public partial class Datos_Usuario : Form
+    {
+        public Datos_Usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
