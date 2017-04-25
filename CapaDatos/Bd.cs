71ccdc7e8c4f01861dc56e97c7fdfe02d6496e34
pablo cesae leyva ramir <pablo.pclr@hotmail.com>
2017-04-25 12:53:31 -0600

    dfsdf

--- CapaDatos/Bd.cs
+++ CapaDatos/Bd.cs
@@ -7,7 +7,7 @@ using MySql.Data.MySqlClient;
 using System.Collections;
 using System.Data;
 using System.Configuration;
-//hola soy Michel X2
+//hola soy Michel X3
 namespace CapaDatos
 {
    public  class Bd
