190830640cc548648403bb1caba9030ea27dc087
Frank Crow-Belloso <Frank Crow-Belloso@ASUS>
2017-06-13 12:32:50 -0600

    Inventario

---
+++ CapaPresentacion2/Modulo_Empleados/INVENTARIO/Mensajes/AltaBaja.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaLogica;
+
+namespace Modulo_Inventario.mensajes
+{
+    public partial class AltaBaja : Form
+    {
+        Producto p = new Producto();
+        public AltaBaja( string valor)
+        {
+            InitializeComponent();
+            try
+            {
+                consultaproducto();
+                txtCodigo.Text = valor;
+                txtNombre.Text = d.Rows[0][0].ToString();
+                txteActual.Text = d.Rows[0][1].ToString();
+            }
+            catch (Exception e) {
+                MessageBox.Show("Error consulte con su administrador");
+            }
+
+
+
+        }
+        DataTable d;
+        void consultaproducto() {
+           d= p.EstatusbajaAlta("24");
+        }
+        string estado;
+        void actualizar() {
+            if (cbEstatus.SelectedItem != null) {
+               estado = cbEstatus.SelectedItem.ToString().Substring(0, 1);
+            }
+
+            
+            if (cbEstatus.SelectedItem != null && estado != txteActual.Text)
+            {
+                try
+                {
+
+                    p.ModificarAltaBaja(txtCodigo.Text,estado );
+                    MessageBox.Show("Se modifico correctamtente");
+                    this.Close();
+                }
+                catch (Exception e)
+                {
+
+
+                    MessageBox.Show("Error consulte con su administrador");
+                }
+            }
+            else {
+                MessageBox.Show("Seleccione un estado que sea diferente al actual para continuar");
+            }
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mensajeConfirmacion mc = new mensajeConfirmacion("Desea salir?","SI F1", "NO F2","");
+            mc.ShowDialog();
+            if (mc.valor) {
+
+                this.Close();
+            }
+        }
+
+        private void AltaBaja_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == (int)Keys.A)
+            {
+
+                actualizar();
+            }
+            if ((int)e.KeyChar == (int)Keys.F2)
+            {
+                mensajeConfirmacion mc = new mensajeConfirmacion("Desea salir?", "SI F1", "NO F2", "");
+                mc.ShowDialog();
+                if (mc.valor)
+                {
+
+                    this.Close();
+                }
+            }
+            if ((int)e.KeyChar == (int)Keys.F3)
+            {
+              
+            }
+        }
+    }
+}
---
+++ CapaPresentacion2/Modulo_Empleados/INVENTARIO/validardouble.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modulo_Inventario
+{
+    public class validardouble
+    {
+
+
+       public  bool ContadoPuntos = false;
+ 
+        public bool SoloNumeros(KeyPressEventArgs e,string t)
+        {
+            //se convierte a Ascci del la tecla presionada 
+
+            int ascci = Convert.ToInt32(Convert.ToChar(e.KeyChar));
+            //verificamos que se encuentre en ese rango que son entre el 0 y el 9 
+      
+           
+                if (t.Contains(".") != true) {
+                    ContadoPuntos = false;
+                }
+                if (ContadoPuntos != true && t.Contains(".") != true)
+                {
+                    if ((ascci >= 48 && ascci <= 57) || ascci == 46 || ascci == 8)
+                    {
+
+                        if (ascci == 46)
+                        {
+                        if (t.Length ==0)
+                        {
+
+                            MessageBox.Show("No se puede ingresar el punto en la primera posicion");
+                            return true;
+                        }
+                     
+                  
+                            ContadoPuntos = true;
+                            return false;
+                        
+
+                        }
+                        return e.Handled = false;
+                    }
+                    else
+                    {
+                        return e.Handled = true;
+                    }
+                }
+                else
+                {
+
+
+                    if ((ascci >= 48 && ascci <= 57) || ascci == 46 || ascci == 8)
+                    {
+
+                        if (ascci == 46)
+                        {
+                            MessageBox.Show("Solo se puede un punto en la cifra borralo para colocarlo en otra parte", "Mensaje");
+                            return e.Handled = true;
+
+                        }
+                        else {
+
+                            return e.Handled = false;
+                        }
+
+
+                    }
+                    else
+                    {
+                        return e.Handled = true;
+                    }
+
+                }
+            //}
+            //else {
+            //    MessageBox.Show("No se puede ingresar el punto en la primera posicion");
+            //    return true;
+            //}
+        }
+    }
+}
