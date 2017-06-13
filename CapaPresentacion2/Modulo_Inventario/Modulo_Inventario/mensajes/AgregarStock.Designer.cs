5a7887f3a72d06f99ef4014ddab8be5facb4b715
pablo cesae leyva ramir <pablo.pclr@hotmail.com>
2017-06-13 00:30:15 -0600

    Inventario falta cargar imagenes y provar las teclas

---
+++ CapaPresentacion2/Modulo_Inventario/Modulo_Inventario/mensajes/AgregarStock.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Modulo_Inventario.mensajes
+{
+    partial class AgregarStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgregarStock));
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtstockActual = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtañadirProduct = new System.Windows.Forms.MaskedTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(156, 105);
+            this.txtCodigo.MaxLength = 20;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(220, 20);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCodigo_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SALIR F2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Codigo del Producto:*";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Guardar F1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Añadir Producto:*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "StockActual";
+            // 
+            // txtstockActual
+            // 
+            this.txtstockActual.Enabled = false;
+            this.txtstockActual.Location = new System.Drawing.Point(245, 171);
+            this.txtstockActual.Name = "txtstockActual";
+            this.txtstockActual.Size = new System.Drawing.Size(131, 20);
+            this.txtstockActual.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Añadir  Mas Producto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nombre Producto:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(156, 131);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(220, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtañadirProduct
+            // 
+            this.txtañadirProduct.Location = new System.Drawing.Point(246, 213);
+            this.txtañadirProduct.Name = "txtañadirProduct";
+            this.txtañadirProduct.Size = new System.Drawing.Size(130, 20);
+            this.txtañadirProduct.TabIndex = 1;
+            this.txtañadirProduct.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtañadirProduct_KeyPress);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(429, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 36);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "LIMPIAR CAMPOS F3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(429, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(93, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AgregarStock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(551, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtañadirProduct);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtstockActual);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCodigo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "AgregarStock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AgregarStock";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AgregarStock_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtstockActual;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.MaskedTextBox txtañadirProduct;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ CapaPresentacion2/Modulo_Inventario/Modulo_Inventario/mensajes/mensajeConfirmacion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modulo_Inventario.mensajes
+{
+    public partial class mensajeConfirmacion : Form
+    {
+
+        public bool valor { get; set; }
+        string bt11, bt22, textt, stock;
+
+        private void mensajeConfirmacion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if ((int)e.KeyChar == (int)Keys.F1)
+            {
+
+                valor = true;
+                this.Close();
+            }
+            if ((int)e.KeyChar == (int)Keys.F2)
+            {
+                valor = false;
+                this.Close();
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            valor = true;
+            this.Close();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            valor = false;
+            this.Close();
+        }
+
+        public mensajeConfirmacion(string texto,string b1, string b2,string valorr)
+        {
+            InitializeComponent();
+            label1.Text = texto+valorr;
+            button1.Text = b1;
+            button2.Text = b2;
+
+
+            
+            
+        }
+
+    }
+}
